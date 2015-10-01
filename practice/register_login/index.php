e0a090f6a49029ac1502331049cbe1ae873723a4
amunikeb <amunikeb@yahoo.com>
2015-10-01 09:10:49 -0400

    made a minor change to the index page

--- practice/register_login/index.php
+++ practice/register_login/index.php
@@ -1,8 +1,6 @@
 <?php 
 include 'core/init.php'; 
-include 'includes/overall/header.php'; 
-
-?>
+include 'includes/overall/header.php'; ?>
       <h1>Home</h1>
       <p>Just a template.</p>
 <?php include 'includes/overall/footer.php'; ?>    
