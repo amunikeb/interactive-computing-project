1ad7547179fb2274025726d87a5fbad31450fc4a
amunikeb <amunikeb@yahoo.com>
2015-10-02 08:27:12 -0400

    modified register.php

--- practice/register_login/register.php
+++ practice/register_login/register.php
@@ -1,4 +1,6 @@
-<?php include 'includes/overall/header.php'; ?>
+<?php
+include 'core/init.php';
+include 'includes/overall/header.php'; ?>
       <h1>Register</h1>
       <p>Registration Page</p>
 <?php include 'includes/overall/footer.php'; ?>    
